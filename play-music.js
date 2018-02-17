6c583739943d465685a3b950f9b03bccd35000ab
Geek Rodion <rodionaaafgh@gmail.com>
2018-02-17 18:39:01 +0300

    feature: network issues handling

--- play-music.js
+++ play-music.js
@@ -1,16 +1,34 @@
 const puppeteer = require('puppeteer')
 
-module.exports = async url => {
+const playMusic = async url => {
   const browser = await puppeteer.launch({ headless: false })
-  const page = await browser.newPage()
-  await page.goto(url)
+  try {
+    const page = await browser.newPage()
+    page.setViewport({ width: 1920, height: 1080})
+    await page.goto(url)
+    const playButton = await page.$('.button-play')
+    playButton.click()
 
-  const playButton = await page.$('.button-play')
-  playButton.click()
+    const shuffleButton = await page.$('.player-controls__btn_shuffle')
+    shuffleButton.click()
 
-  const shuffleButton = await page.$('.player-controls__btn_shuffle')
-  shuffleButton.click()
-
-  const nextButton = await page.$('.player-controls__btn_next')
-  nextButton.click()
+    const nextButton = await page.$('.player-controls__btn_next')
+    nextButton.click()
+  } catch(err) {
+    browser.close()
+    throw err
+  }
 }
+
+const playMusicWithHandle = async (url, attempt = 0) => {
+  if (attempt < 5) {
+    try {
+      await playMusic(url)
+    } catch(_) {
+      await new Promise(res => setTimeout(res, 5000))
+      await playMusicWithHandle(url, attempt + 1)
+    }
+  }
+}
+
+module.exports = playMusicWithHandle
